98e5c50b7154e5825389652e37cc3621eb66a782
Марина Максимова <m.maksimova@m-maksimova.local>
2022-10-18 18:49:59 +0300

    Старт проекта

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './components/Card/Card';
+import Input from './components/Input/Input';
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+      <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,9 @@
+import './Input.css';
+
+export default function Input(props) {
+    return (
+        <div>
+            <input className="input" type="text" />
+        </div>
+    );
+}
